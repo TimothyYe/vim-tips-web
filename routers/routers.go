fd351784eeba18503a3d909c31fda33c2b2e3e4d
Timothy <yexiaozhou2003@gmail.com>
2014-06-25 17:30:10 +0800

    adjust project structure

--- routers/routers.go
+++ routers/routers.go
@@ -1,5 +1,38 @@
 package routers
 
-func InitRouters() {
+import (
+	"fmt"
+	"github.com/codegangsta/martini-contrib/render"
+	"github.com/go-martini/martini"
+	"net/http"
+)
 
+var (
+	m = martini.Classic()
+)
+
+func Init() {
+	m.Use(render.Renderer(render.Options{
+		Directory:  "templates",
+		Extensions: []string{".tpl", ".html"},
+		Charset:    "UTF-8",
+	}))
+
+	InitRouters()
+	http.Handle("/", m)
+
+	fmt.Println("Server started...")
+
+	err := http.ListenAndServe("127.0.0.1:3001", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func Hello(r render.Render) {
+	r.HTML(200, "hello", "Timothy")
+}
+
+func InitRouters() {
+	m.Get("/", Hello)
 }
--- web.go
+++ web.go
@@ -1,40 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"github.com/codegangsta/martini-contrib/render"
-	"github.com/go-martini/martini"
 	"github.com/timothyye/vim-tips-web/routers"
-	"net/http"
 )
-
-var (
-	m = martini.Classic()
-)
-
-func initEnv() {
-	m.Use(render.Renderer(render.Options{
-		Directory:  "templates",
-		Extensions: []string{".tpl", ".html"},
-		Charset:    "UTF-8",
-	}))
-
-	routers.InitRouters()
-}
-
-func Hello(r render.Render) {
-	r.HTML(200, "hello", "Timothy")
-}
 
 func main() {
-	initEnv()
-
-	m.Get("/", Hello)
-	http.Handle("/", m)
-	fmt.Println("Server started...")
-
-	err := http.ListenAndServe("127.0.0.1:3001", nil)
-	if err != nil {
-		fmt.Println(err)
-	}
+	routers.Init()
 }
