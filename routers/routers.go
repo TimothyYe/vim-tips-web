7938e62b9b4d04043422460abb084b7b85b47754
Timothy <yexiaozhou2003@gmail.com>
2014-06-30 19:18:27 +0800

    add DB handler.

--- routers/routers.go
+++ routers/routers.go
@@ -1,9 +1,18 @@
 package routers
 
 import (
+	"fmt"
 	"github.com/codegangsta/martini-contrib/render"
 	"github.com/go-martini/martini"
 )
+
+func DB() martini.Handler {
+	fmt.Println("DB handler is called...1")
+
+	return func() {
+		fmt.Println("DB handler is called...2")
+	}
+}
 
 func Initialize(m *martini.ClassicMartini) {
 	m.Use(render.Renderer(render.Options{
@@ -12,6 +21,8 @@ func Initialize(m *martini.ClassicMartini) {
 		Extensions: []string{".tpl", ".html"},
 		Charset:    "UTF-8",
 	}))
+
+	m.Use(DB)
 
 	InitRouters(m)
 }
