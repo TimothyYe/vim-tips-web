8c98c5553bd0f0f765b360b5f9cdd93c1a4448a8
Timothy <yexiaozhou2003@gmail.com>
2014-07-24 16:43:13 +0800

    update websocket in api page

---
+++ public/js/ws.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+	connectServer();
+});
+
+function connectServer()
+{
+	var sock = null;
+	var wsuri = "ws://127.0.0.1:3001/ws";
+
+	try
+	{
+		sock = new WebSocket(wsuri);
+	}catch (e) {
+	}
+
+	sock.onopen = function() {
+		console.log("connected to " + wsuri);
+	};
+
+	sock.onerror = function(e) {
+		console.log(" error from connect " + e);
+	};
+
+	sock.onclose = function(e) {
+		console.log("connection closed (" + e.code + ")");
+	};
+
+	sock.onmessage = function(e) {
+		console.log("message received: " + e.data);
+
+		$("#msg").text(e.data);
+	};
+}
