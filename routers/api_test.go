a58403bfd603793732402902691f586c48f10d08
Timothy <yexiaozhou2003@gmail.com>
2014-07-25 17:04:00 +0800

    add test case for api router

---
+++ routers/api_test.go
@@ -0,0 +1,16 @@
+package gotest
+
+import (
+	"labix.org/v2/mgo"
+	"testing"
+)
+
+func TestHandleRandomTxtTip(t *testing.T) {
+	session, err := mgo.Dial("mongodb://localhost")
+	if err != nil {
+		t.Error("Failed to connect to mongo DB...")
+	} else {
+		t.Log("Connected to mongo DB...")
+	}
+
+}
