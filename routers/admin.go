bd1ca879a4abc23d1f7263998631ce517bb728e6
Timothy <yexiaozhou2003@gmail.com>
2014-09-13 15:32:13 +0800

    update login logic

--- routers/admin.go
+++ routers/admin.go
@@ -7,6 +7,14 @@ import (
 	"net/http"
 )
 
+func validateSession(r render.Render, s sessions.Session, retUrl string) {
+	isLogin := s.Get("IsLogin")
+
+	if isLogin == nil {
+		r.Redirect("/admin/login")
+	}
+}
+
 func ShowLoginPage(r render.Render) {
 	r.HTML(200, "admin/login", map[string]interface{}{
 		"IsAbout": true}, render.HTMLOptions{})
@@ -18,6 +26,7 @@ func HandleLogin(req *http.Request, r render.Render, s sessions.Session) {
 
 	if username == "aa@bb.com" && pass == "111" {
 		fmt.Println("Login success!")
+		s.Set("IsLogin", true)
 
 		r.Redirect("/admin/index")
 	} else {
@@ -26,6 +35,8 @@ func HandleLogin(req *http.Request, r render.Render, s sessions.Session) {
 }
 
 func HandleAdminIndex(r render.Render, s sessions.Session) {
+	validateSession(r, s, "/admin/login")
+
 	r.HTML(200, "admin/index", map[string]interface{}{
 		"IsAbout": true}, render.HTMLOptions{Layout: "admin/layout"})
 }
