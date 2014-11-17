7408ba36be62a7aa0c021a9600ca61343941934c
Timothy <yexiaozhou2003@gmail.com>
2014-11-17 23:01:14 +0800

    bug fix

--- models/cast.go
+++ models/cast.go
@@ -4,8 +4,8 @@ import (
 	"labix.org/v2/mgo/bson"
 )
 
-type API struct {
-	Id    bson.ObjectId `bson:"_id,omitempty"`
-	Type  string
-	Count uint64
+type Casts struct {
+	Id         bson.ObjectId `bson:"_id,omitempty"`
+	Author     string
+	VisitCount int
 }
