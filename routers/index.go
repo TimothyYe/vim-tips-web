6d9bb908212781e4cb2874fd3f5ab7fa74e778ea
Timothy <yexiaozhou2003@gmail.com>
2015-03-16 21:43:24 +0800

    bug fix

--- routers/index.go
+++ routers/index.go
@@ -1,11 +1,12 @@
 package routers
 
 import (
+	"math/rand"
+	"time"
+
 	"github.com/codegangsta/martini-contrib/render"
 	"github.com/timothyye/vim-tips-web/models"
 	"labix.org/v2/mgo"
-	"math/rand"
-	"time"
 )
 
 func getRandomIndex(total int) int {
@@ -17,6 +18,11 @@ func HandleIndex(r render.Render, db *mgo.Database) {
 	tip := models.Tips{}
 	total, _ := db.C("tips").Count()
 	index := getRandomIndex(total)
+
+	if index == total {
+		index = total - 1
+	}
+
 	db.C("tips").Find(nil).Skip(index).One(&tip)
 
 	r.HTML(200, "index", map[string]interface{}{
